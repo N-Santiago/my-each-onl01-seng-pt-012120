874026100e9d5036660f2d4a984cc1a099bf8dc1
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 03:56:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  n = 0
+  while n < array.length
+  yield array[n]
+  n+=1   
+end
+return array
+end
+array = [1,2,3]
+my_each(array) do |x|
+  puts x
+end 
 end
