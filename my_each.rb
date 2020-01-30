2982a15fe7491590175addb209681f945e61851c
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-30 03:57:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,12 +2,12 @@ def my_each(array)
   n = 0
   while n < array.length
   yield array[n]
-  n+=1   
+  n += 1   
 end
 return array
 end
 array = [1,2,3]
-my_each(array) do |x|
+def my_each(array) do |x|
   puts x
 end 
 end
